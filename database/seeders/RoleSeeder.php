5d03e951d95322724d07ea686d163ccbe13984fc
sting <sik77mo@gmail.com>
2021-11-20 23:27:17 -0500

    update role user

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role1 = Role::create(['name' => 'Admin']);
+        $role2 = Role::create(['name' => 'Businessman']);
+        $role3 = Role::create(['name' => 'Client']);
+
+        Permission::create(['name' => 'admin.home'])->assignRole($role1);
+
+        Permission::create(['name' => 'businessman.home'])->assignRole($role2);
+
+        Permission::create(['name' => 'client.home'])->assignRole($role3);
+    }
+}
